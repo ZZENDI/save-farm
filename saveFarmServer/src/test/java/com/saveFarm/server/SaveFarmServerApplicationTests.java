90938fba4d4eecd48939480c5e464d2bc1cd1c9b
hhjkhhbgkugjnbj1 <ck8901ck@gmail.com>
2024-09-27 03:48:41 +0900

    server main file push

---
+++ saveFarmServer/src/test/java/com/saveFarm/server/SaveFarmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saveFarm.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveFarmServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
